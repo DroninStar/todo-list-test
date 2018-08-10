1b4443ae8a6b6fe83bc7cd8ec7d0bab472702505
DroninStar <dronin.star@hotmail.com>
2018-08-10 17:03:16 +0300

    Adding pages

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
     return view('home');
 });
+
 Route::get('/contact', function () {
     return view('contact');
 });
+
 Route::get('/about', function () {
     return view('about');
 });
